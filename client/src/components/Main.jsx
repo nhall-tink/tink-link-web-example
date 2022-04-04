4b61dd8b6ad58ac7d0b5b27074b8e253c5f7e450
Nathan Hall <nathan.hall@tink.com>
2022-04-04 14:48:07 +0100

    feat: new landing page

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -8,17 +8,21 @@ export const Main = () => {
   const [locale, setLocale] = useState("en_US");
   return (
     <div>
-      <Header text="Hello!" emoji="money" />
-      <p>Welcome, {process.env.REACT_APP_BRAND_NAME}!</p>
-      <p>We can help you analyze your financial status.</p>
+      <Header text="Welcome, {process.env.REACT_APP_BRAND_NAME}!" emoji="money" />
+      <p>Congrats on deploying the demo application! We're almost ready to do something awesome...</p>
+      {/* <p>We can help you analyze your financial status.</p>
       <p>
         Or actually we can’t. We’re just a simple example app. But you can
         connect your bank to see your account data, transactions and
         investments!
-      </p>
-
-      <p>You can share the App with anyone you choose to in your company, to allow them to see the potential power of our companies partnership!</p>
-
+      </p> */}
+      <hr></hr>
+      <p><b>Next steps:</b></p>
+      <p>To get the demo working, you need to register the redirect URI for our example app
+      <p><code>{window.location.href}callback</code></p> in the list of redirect URIs under your <a href='https://console.tink.com/app-settings/client'>app's settings</a></p>
+      <hr></hr>
+      <h2>Test it out!</h2>
+      <p>You're now ready to test out our experience, pick a market and a locale and connect your bank!</p>
       <div style={{ padding: "50px 0 10px 0" }}>
         <BasicDropdown
           name="Choose a market"
@@ -61,13 +65,16 @@ export const Main = () => {
           style={{ marginBottom: "30px" }}
         />
       </div>
-
       <AuthorizationLink
         scope="accounts:read,investments:read,transactions:read,user:read"
         market={market}
         locale={locale}
       />
+      <hr></hr>
+      <h2>Share the experience!</h2>
+      <p>You can share the App with anyone in your company, to allow them to see the potential power of our companies partnership!</p>
     </div>
+    
   );
 };
 
